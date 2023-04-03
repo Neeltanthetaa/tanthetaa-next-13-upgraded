6d776c252e6c6f480e38a07b81e6e483ae10a9fe
yagniktanthetaa <yagnik@tanthetaa.co.in>
2023-04-03 11:07:46 +0530

    new components make

--- app/pages/nfts/[nft]/page.js
+++ app/pages/nfts/[nft]/page.js
@@ -40,100 +40,100 @@ const NFT = async ({ params }) => {
       {/* 1st Component Start */}
       <NFTHead
         path={`nfts/nft-development-services`}
-        content={content.data.header}
+        content={content?.data?.header}
       />
       {/* 1st Component End */}
 
       {/* 2nd Component Start */}
-      <NFTDevelopment content={content.data.section1} />
+      <NFTDevelopment content={content?.data?.section1} />
       {/* 2nd Component End */}
 
       {/* 3rd Component Start */}
-      <NFTDevelopment content={content.data.section2} />
+      <NFTDevelopment content={content?.data?.section2} />
       <TokenDevelopment
         path={`nfts/nft-development-services`}
-        content={content.data.section2.services}
+        content={content?.data?.section2?.services}
       />
       {/* 3rd Component End */}
 
       {/* 4th Component Start */}
-      <NFTDevelopment content={content.data.section3} />
+      <NFTDevelopment content={content?.data?.section3} />
       <NFTProcess
         path={`nfts/nft-development-services`}
-        content={content.data.section3.services}
+        content={content?.data?.section3?.services}
       />
       {/* 4th Component End */}
 
       {/* 5th Component Start */}
-      <NFTOffers content={content.data.section4} />
+      <NFTOffers content={content?.data?.section4} />
       {/* In Progress */}
       {/* 5th Component End */}
 
       {/* 6th Component Start */}
-      <NFTDevelopment content={content.data.section5} />
+      <NFTDevelopment content={content?.data?.section5} />
       <NFTFractures
         path={`nfts/nft-development-services`}
-        content={content.data.section5}
+        content={content?.data?.section5}
       />
       {/* 6th Component End */}
 
       {/* 7th Component Start */}
-      <NFTDevelopment content={content.data.section6} />
-      <NFTPackages content={content.data.section6.services} />
+      <NFTDevelopment content={content?.data?.section6} />
+      <NFTPackages content={content?.data?.section6?.services} />
       {/* 7th Component End */}
 
       {/* 8th Component Start */}
-      <NFTDevelopment content={content.data.section7} />
+      <NFTDevelopment content={content?.data?.section7} />
       <NFTPartners
         path={`nfts/nft-development-services`}
-        content={content.data.section7.services}
+        content={content?.data?.section7?.services}
       />
       {/* 8th Component End */}
 
       {/* 9th Component Start */}
       <NFTClients
         path={`nfts/nft-development-services`}
-        content={content.data.section8}
+        content={content?.data?.section8}
       />
       {/* 9th Component End */}
 
       {/* 10th Component Start */}
-      <NFTDevelopment content={content.data.section9} />
+      <NFTDevelopment content={content?.data?.section9} />
       <NFTSpotlighted
         path={`nfts/nft-development-services`}
-        content={content.data.section9.services}
+        content={content?.data?.section9?.services}
       />
       {/* 10th Component End */}
 
       {/* 11th Component Start */}
-      <NFTDevelopment content={content.data.section10} />
-      <NFTBusiness content={content.data.section10.services} />
+      <NFTDevelopment content={content?.data?.section10} />
+      <NFTBusiness content={content?.data?.section10?.services} />
       {/* 11th Component End */}
 
       {/* 12th Component Start */}
       <NFTService
         path={`nfts/nft-development-services`}
-        content={content.data.section11}
+        content={content?.data?.section11}
       />
       {/* 12th Component End */}
 
       {/* 13th Component Start */}
-      <NFTDevelopment content={content.data.section12} />
-      <Faqs content={content.data.section12.services} />
+      <NFTDevelopment content={content?.data?.section12} />
+      <Faqs content={content?.data?.section12?.services} />
       {/* 13th Component End */}
 
       {/* 14th Component Start */}
-      <NFTDevelopment content={content.data.section13} />
+      <NFTDevelopment content={content?.data?.section13} />
       <Blogs
         path={`nfts/nft-development-services`}
-        content={content.data.section13.services}
+        content={content?.data?.section13?.services}
       />
       {/* 14th Component End */}
 
       {/* 15th Component Start */}
       <PreviousOrNext
         path={`nfts/nft-development-services`}
-        content={content.data.section14.services}
+        content={content?.data?.section14?.services}
       />
       {/* 15th Component End */}
 
