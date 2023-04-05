645905034f0fb48f9891d824172983a99e42d55a
Uday Tanthetaa <uday@tanthetaa.co.in>
2023-04-05 11:56:22 +0530

    seo updated for blog

--- app/test/blog/[slug]/page.jsx
+++ app/test/blog/[slug]/page.jsx
@@ -1,20 +1,53 @@
 import { notFound } from "next/navigation";
 
+// apiUrl without plugin
+// `${process.env.NEXT_PUBLIC_BLOG_API_URL}/api/slugify/slugs/blog/hello-world-in-lorem-ipsum-style?populate[seo][populate]=*`,
+
 export const getBlogContent = async ({ slug }) => {
 	try {
 		const res = await fetch(
-			`${process.env.NEXT_PUBLIC_BLOG_API_URL}/api/slugify/slugs/blog/hello-world-in-lorem-ipsum-style?populate=*`,
+			`${process.env.NEXT_PUBLIC_BLOG_API_URL}/api/slugify/slugs/blog/hello-world-in-lorem-ipsum-style?populate=deep`,
 			{
 				headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPER_TOKEN}` },
 			}
 		);
 		const data = await res.json();
-
-		console.log(data);
 		return data;
 	} catch (err) {
 		console.log(err);
 	}
+};
+
+export const generateMetadata = async ({ params }) => {
+	const slug = params.slug;
+	const content = await getBlogContent({ slug });
+	if (!content) return;
+	const seo = content.data.attributes.seo;
+
+	return {
+		title: seo.metaTitle,
+		description: seo.metaDescription,
+		keywords: seo.keywords,
+		openGraph: {
+			type: "article",
+			title: seo.metaTitle,
+			description: seo.metaDescription,
+			images: [
+				{
+					...seo.metaImage.data.attributes,
+					url: `${process.env.NEXT_PUBLIC_BLOG_API_URL}${seo.metaImage.data.attributes.url}`,
+					alt: seo.metaImage.data.attributes.alternativeText,
+					alternativeText: null,
+					formats: null,
+				},
+			],
+			...seo.metaArticle,
+			author: {
+				id: null,
+			},
+			locale: "en-US",
+		},
+	};
 };
 
 const Test = async ({ params }) => {
